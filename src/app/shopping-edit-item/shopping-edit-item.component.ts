98a8d7b54845d94c96533713832e62f0a516d18b
Jugal <jkj.619@gmail.com>
2019-01-09 11:21:35 +0530

    c

---
+++ src/app/shopping-edit-item/shopping-edit-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-edit-item',
+  templateUrl: './shopping-edit-item.component.html',
+  styleUrls: ['./shopping-edit-item.component.css']
+})
+export class ShoppingEditItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
